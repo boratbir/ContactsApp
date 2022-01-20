7ca4d2ad5d417f8e67535f3ad1e4c74ba1c133b1
boratbir <boratbir@yandex.ru>
2022-01-20 20:39:33 +0700

    <ContactsApp.sln>:Создано решение и проекты.

---
+++ ContactsApp.UI/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ContactsApp.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
