fbaea0c0284c7f556d22836c4f073660a7e1cfd7
boratbir <boratbir@yandex.ru>
2022-01-22 18:34:08 +0700

    <Project.cs>:Создан класс Project.

--- ContactsApp/Project.cs
+++ ContactsApp/Project.cs
@@ -1,6 +1,18 @@
-﻿namespace ContactsApp
+﻿using System.Collections.Generic;
+using System.Linq;
+using ContactsApp.Models;
+
+namespace ContactsApp
 {
     public class Project
     {
+        /// <summary>
+        /// Список контактов.
+        /// </summary>
+        public List<Contact> Contacts { get; } = new List<Contact>();
+
+        public List<Contact> Find(string substring) => Contacts
+            .Where(i => i.LastName.StartsWith(substring))
+            .ToList();
     }
 }
