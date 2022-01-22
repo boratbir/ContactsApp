cec2d73579db2ce9c3dffc92a737d29bee066df1
boratbir <boratbir@yandex.ru>
2022-01-22 18:18:12 +0700

    <PhoneNumber.cs>:Создан класс номера телефона.

---
+++ ContactsApp/Models/PhoneNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ContactsApp.Models
+{
+    /// <summary>
+    /// Номер телефона контакта.
+    /// </summary>
+    public class PhoneNumber
+    {
+        /// <summary>
+        /// Поле номера телефона.
+        /// </summary>
+        private long _phoneNumber = 70000000000;
+
+        /// <summary>
+        /// Номер телефона.
+        /// </summary>
+        /// <exception cref="ArgumentException">Номер телефона не соответствует российскому формату.</exception>
+        public long Number
+        {
+            get => _phoneNumber;
+            set
+            {
+                if (value < 70000000000 || value > 79999999999)
+                    throw new ArgumentException("Номер должен состоять из 11 цифр и начинаться с 7.");
+                _phoneNumber = value;
+            }
+        }
+    }
+}
