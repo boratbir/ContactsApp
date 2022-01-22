199e5824296db39c80dfbca08e0806975e8500ac
boratbir <boratbir@yandex.ru>
2022-01-22 19:07:27 +0700

    <ProjectManager.cs>:Создан класс менеджера проекта, написаны методы для работы с файлами.

--- ContactsApp/ProjectManager.cs
+++ ContactsApp/ProjectManager.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using File = System.IO.File;
 
 namespace ContactsApp
 {
-    public class ProjectManager
+    /// <summary>
+    /// Менеджер проекта, с помощью которого осуществляется чтение и запись
+    /// списка контактов на диск.
+    /// </summary>
+    public static class ProjectManager
     {
         /// <summary>
         /// Путь до папки AppData текущего пользователя.
@@ -21,5 +29,79 @@ namespace ContactsApp
         /// </summary>
         private static string ProjectFilePath =>
             ContactsAppFolder + "Contacts.notes";
+
+        /// <summary>
+        /// Читает текст из файла.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>Текст в кодировке UTF8.</returns>
+        /// <exception cref="Exception">Произошла ошибка чтения.</exception>
+        private static string ReadFile(string path)
+        {
+            try
+            {
+                var bytes = File.ReadAllBytes(path);
+                var text = Encoding.UTF8.GetString(bytes);
+                return text;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось прочитать файл проекта.", e);
+            }
+        }
+
+        /// <summary>
+        /// Записывает текст в файл. Если файла нет, то он будет создан.
+        /// Если есть, то его содержимое будет перезаписано.
+        /// </summary>
+        /// <param name="text">Текст.</param>
+        /// <param name="path">Путь до файла.</param>
+        /// <exception cref="Exception">Произошла ошибка записи.</exception>
+        private static void WriteFile(string text, string path)
+        {
+            try
+            {
+                var bytes = Encoding.UTF8.GetBytes(text);
+                File.WriteAllBytes(path, bytes);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось записать файл.", e);
+            }
+        }
+        
+        /// <summary>
+        /// Загружает проект из файла.
+        /// </summary>
+        /// <returns>Объект проекта со списком контактов.</returns>
+        /// <exception cref="Exception">Произошла ошибка чтения файла проекта.</exception>
+        public static Project ReadProject()
+        {
+            try
+            {
+                // Папка проекта создаётся, если её не существует.
+                Directory.CreateDirectory(ContactsAppFolder);
+                var text = ReadFile(ProjectFilePath);
+                var project = JsonConvert.DeserializeObject<Project>(text);
+                return project;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Произошла ошибка чтения списка контактов.", e);
+            }
+        }
+
+        public static void SaveProject(this Project project)
+        {
+            try
+            {
+                var text = JsonConvert.SerializeObject(project);
+                WriteFile(text, ProjectFilePath);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось записать файл проекта.", e);
+            }
+        }
     }
 }
